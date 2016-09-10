7dfae0acbd7bd7547bc929901e882f5cc9dfb3c7
Matt Velez <mzvelez75@gmail.com>
2016-09-10 13:40:04 -0400

    Finalize notOverlapping

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var draw = SVG('map').size(500, 500);
   var stars = draw.set();
   var i = 0;
-  while (i < 50) {
+  while (i < 30) {
     x = getRandomInt(10, 490);
     y = getRandomInt(10, 490);
 
@@ -17,14 +17,14 @@ $(document).ready(function() {
 var notOverlapping = function(set, x, y) {
   var b
   set.each(function() {
-    if      (this.inside(x - 5, y - 5)) { b = false; }
-    else if (this.inside(x + 5, y - 5)) { b = false; }
-    else if (this.inside(x - 5, y + 5)) { b = false; }
-    else if (this.inside(x + 5, y + 5)) { b = false; }
-    else if (this.inside(x, y - 5))     { b = false; }
-    else if (this.inside(x, y + 5))     { b = false; }
-    else if (this.inside(x + 5, y))     { b = false; }
-    else if (this.inside(x - 5, y))     { b = false; }
+    if      (this.inside(x - 7, y - 7)) { b = false; }
+    else if (this.inside(x + 7, y - 7)) { b = false; }
+    else if (this.inside(x - 7, y + 7)) { b = false; }
+    else if (this.inside(x + 7, y + 7)) { b = false; }
+    else if (this.inside(x, y - 7))     { b = false; }
+    else if (this.inside(x, y + 7))     { b = false; }
+    else if (this.inside(x + 7, y))     { b = false; }
+    else if (this.inside(x - 7, y))     { b = false; }
   });
   return b === undefined ? true : b;
 }
