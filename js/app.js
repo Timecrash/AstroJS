81bef13444e456ece3c899acd07779f011f8fd4c
Matt Velez <mzvelez75@gmail.com>
2016-09-10 10:24:57 -0400

    Displays name when moused-over

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
-var getRandomInt = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 $(document).ready(function() {
   var draw = SVG('map').size(500, 500);
   for (i = 0; i < 20; i++) {
     x = getRandomInt(10, 490);
     y = getRandomInt(10, 490);
 
-    draw.circle(10).attr({cx: x, cy: y});
+    draw.circle(10).attr({cx: x, cy: y, name: "Delta Pavonis"}).addClass('star');
+    $('.star').hover(function() {
+      draw.text($(this).attr('name'));
+    })
   }
 });
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 /**
 var star = draw.circle(40).attr({fill: "green"}).addClass("star");
