93bc37e096f7fcc305077c7cd877431ce1e1a532
Matt Velez <mzvelez75@gmail.com>
2016-09-10 09:57:47 -0400

    Playing around

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
   var draw = SVG('map').size(500, 500);
-  var star = draw.circle(40).fill(gradient);
+  var star = draw.circle(40).attr({fill: "green"}).addClass("star");
+  var star2 = draw.circle(50).attr(
+    {fill: "steelblue",
+     cx: 480,
+     cy: 480,
+     name: "Delta Pavonis"}).addClass("star");
+  $(".star").hover(function() {
+    var name = $($(this).attr('name'));
+    map.append(name).slideToggle();
+  });
 });
